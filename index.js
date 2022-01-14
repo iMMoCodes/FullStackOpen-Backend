db514b5d761641fb35bd96d4470e960637afd1bf
iMMoCodes <matti-makela@hotmail.com>
2022-01-14 03:09:05 +0200

    Make it better

--- index.js
+++ index.js
@@ -94,7 +94,8 @@ app.post('/api/persons', (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001;
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST, () => {
   console.log(`Server is running on port ${PORT}`);
 });
