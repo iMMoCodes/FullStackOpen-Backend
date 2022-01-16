7d87cfb4e53a0fda71cbd4bb8f3e99a531fa3582
iMMoCodes <matti-makela@hotmail.com>
2022-01-16 20:30:23 +0200

    Tehtävät 3.15-3.18

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 const Person = require('./models/person');
 
+// MIDDLEWARES
 app.use(express.json());
 morgan.token('data', function (req, res) {
   return JSON.stringify(req.body);
@@ -15,6 +16,7 @@ app.use(
 app.use(cors());
 app.use(express.static('build'));
 
+// PERSON LIST
 let persons = [
   {
     id: 1,
@@ -38,37 +40,53 @@ let persons = [
   },
 ];
 
-app.get('/info', (req, res) => {
-  res.send(`
-    <div>
-    <h2>Phonebook has info for ${persons.length} people</h2>
-    <h2>${new Date()}</h2>
-    </div>
-    `);
+// ROUTES
+// INFO
+app.get('/info', (req, res, next) => {
+  Person.find({})
+    .then((result) => {
+      res.send(`
+      <div>
+      <h2>Phonebook has info for ${result.length} people</h2>
+      <h2>${new Date()}</h2>
+      </div>
+      `);
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+// GET ALL
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+// GET ONE
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+// DELETE
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
+// CREATE
 app.post('/api/persons', (req, res) => {
   const body = req.body;
 
@@ -89,6 +107,35 @@ app.post('/api/persons', (req, res) => {
   });
 });
 
+// UPDATE
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+// ERROR HANDLING
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
+
+// CONNECTION
 const PORT = process.env.PORT;
 const HOST = '0.0.0.0';
 app.listen(PORT, HOST, () => {
